4441ba41deaacdbc5c92c7a4cdcfb1f8ed6d6bf9
Ridge Frederick <ridge.frederick@gmail.com>
2018-06-06 14:22:01 -0500

    Build out Timeular API client via genserver
    Not sure there are any real benefits besides separating out the token management and allowing the client to be 'refreshed' on an unauthorized (expired token) call.

---
+++ lib/timeularex/client.ex
@@ -0,0 +1,62 @@
+defmodule Timeularex.Client do
+  use GenServer
+
+  alias Timeularex.Config
+  alias Timeularex.API
+
+  # Client API
+
+  def start_link() do
+    GenServer.start_link(__MODULE__, :ok, [name: __MODULE__])
+  end
+
+  def request(url, method) when method in [:get, :delete] do
+    GenServer.call(__MODULE__, %{method: method, opts: [url]})
+  end
+
+  def request(url, method, body \\ "") when method in [:patch, :post, :put] do
+    GenServer.call(__MODULE__, %{method: method, opts: [url, body]})
+  end
+
+  # Callbacks
+
+  @impl true
+  def init(_) do
+    case get_access_token() do
+      {:ok, token} -> {:ok, %{token: token}}
+      {_, response} -> {:stop, response}
+    end
+  end
+
+  @impl true
+  def handle_call(%{method: method, opts: opts}, _from, state) do
+    response = apply(API, method, opts ++ [[Authorization: "Bearer #{state.token}"]])
+
+    case response do
+      {:ok, %HTTPoison.Response{body: body, status_code: 200}} -> {:reply, body, state}
+      {:ok, %HTTPoison.Response{body: body, status_code: 404}} -> {:reply, body.message, %{}}
+      {:ok, %HTTPoison.Response{body: body, status_code: 401}} -> {:stop, body.message, %{}}
+      unknown ->
+        IO.inspect unknown
+        {:stop, "unknown response received", %{}}
+    end
+  end
+
+  # Helpers
+
+  defp get_access_token do
+    body = %{
+      apiKey: Config.api_key,
+      apiSecret: Config.api_secret
+    }
+
+    response =
+      ~s(/developer/sign-in)
+      |> API.post(body, [])
+
+    case response do
+      {:ok, %HTTPoison.Response{body: body}} -> {:ok, body["token"]}
+      _ -> {:error, response}
+    end
+  end
+end
